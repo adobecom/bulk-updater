8f7cb62bdaa6fee168472cc16bc932a605ec5a43
Brad Johnson <fullcolorcoder@gmail.com>
2022-12-02 10:38:09 -0600

    Renamed icon labels, and wrote logic to update current mistakes

--- icons.js
+++ icons.js
@@ -14,10 +14,10 @@ import { writeFile } from 'fs/promises';
 import { fetch } from '@adobe/fetch';
 import { mdast2docx } from '@adobe/helix-md2docx';
 import parseMarkdown from '@adobe/helix-html-pipeline/src/steps/parse-markdown.js';
-// import util from 'util';
 
 const entries = [
     'https://main--bacom--adobecom.hlx.page/de/customer-success-stories/accent-group-case-study',
+    'https://main--bacom--adobecom.hlx.page/africa/customer-success-stories/adarsh-credit-cooperative-society-case-study',
 ];
 
 async function main() {
@@ -29,31 +29,41 @@ async function main() {
         await parseMarkdown(state);
 
         const { mdast } = state.content;
-
         let runThrottle = 0;
         mdast.children.forEach(child => {
             if (child.type === 'gridTable' && runThrottle < 1) {
                 if (child.children[0].children[0].children[0].children[0].children[0].value === 'Columns (contained)') {
                     child.children.forEach(grandChild => {
                         if (grandChild.type === 'gtBody') {
-                            grandChild.children[0].children[0].children[0] = {
-                                type: 'paragraph',
-                                children: [
-                                  {
-                                    type: 'text',
-                                    value: ':icon-objectives:',
-                                  }
-                                ]
+                            console.log(grandChild.children[0].children[0].children[0].children[0].type);
+                            if (grandChild.children[0].children[0].children[0].children[0].type === 'text') {
+                                const textArray = grandChild.children[0].children[0].children[0].children[0].value.split(':');
+                                grandChild.children[0].children[0].children[0].children[0].value = `:objectives: ${textArray[2]}`;
+                            } else {
+                                grandChild.children[0].children[0].children[0] = {
+                                    type: 'paragraph',
+                                    children: [
+                                      {
+                                        type: 'text',
+                                        value: ':objectives:',
+                                      }
+                                    ]
+                                }
                             }
-                            grandChild.children[0].children[1].children[0] = {
-                                type: 'paragraph',
-                                children: [
-                                  {
-                                    type: 'text',
-                                    value: ':icon-results:',
-                                  }
-                                ]
-                            }  
+                            if (grandChild.children[0].children[1].children[0].children[0].type === 'text') {
+                                const resultsArray = grandChild.children[0].children[0].children[0].children[0].value.split(':');
+                                grandChild.children[0].children[1].children[0].children[0].value = `:results: ${resultsArray[2]}`;
+                            } else {
+                                grandChild.children[0].children[1].children[0] = {
+                                    type: 'paragraph',
+                                    children: [
+                                    {
+                                        type: 'text',
+                                        value: ':results:',
+                                    }
+                                    ]
+                                }  
+                            }
                         }
                     });
                     runThrottle++;
