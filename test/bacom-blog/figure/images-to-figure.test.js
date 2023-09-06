06c15ac3a20261bcf9941dfd1797a44abdba6087
Brandon Marshall <bmarshal@adobe.com>
2023-09-06 16:52:58 -0700

    MWPW-135376 Blog Bulk Update Images

---
+++ test/bacom-blog/figure/images-to-figure.test.js
@@ -0,0 +1,19 @@
+import { imageToFigure } from '../../../bacom-blog/figure/images-to-figure.js';
+import { readFile } from 'fs/promises';
+import { expect } from '@esm-bundle/chai';
+import { getMdast } from '../../../utils/mdast-utils.js';
+
+describe('imageToFigure', () => {
+  it('should convert images to figures', async () => {
+    const imagesMdPath = new URL('./images-block.md', import.meta.url);
+    const figureMdPath = new URL('./figure-block.md', import.meta.url);
+    const imagesMd = await readFile(imagesMdPath);
+    const figureMd = await readFile(figureMdPath);
+
+    const imagesMdast = await getMdast(imagesMd.toString());
+    const figureMdast = await getMdast(figureMd.toString());
+
+    const mdast = await imageToFigure(imagesMdast);
+    expect(mdast).to.deep.equal(figureMdast);
+  });
+});
