1b2e6891068cffce7fa6f2d744f1a7177a2da87f
Brandon Marshall <bmarshal@adobe.com>
2023-09-07 15:59:26 -0700

    MWPW-135377 Blog Bulk Update Embed

---
+++ test/bacom-blog/embed/embed.test.js
@@ -0,0 +1,32 @@
+import { convertEmbed } from "../../../bacom-blog/embed/embed.js";
+import { readFile } from 'fs/promises';
+import { expect } from '@esm-bundle/chai';
+import { getMdast } from '../../../utils/mdast-utils.js';
+
+describe('embedToLink', () => {
+  it('should convert embeds to links', async () => {
+    const embedMdPath = new URL('./mocks/embed-block-youtube.md', import.meta.url);
+    const linkMdPath = new URL('./mocks/auto-link.md', import.meta.url);
+    const embedMd = await readFile(embedMdPath);
+    const linkMd = await readFile(linkMdPath);
+
+    const embedMdast = await getMdast(embedMd.toString());
+    const linkMdast = await getMdast(linkMd.toString());
+
+    await convertEmbed(embedMdast);
+    expect(embedMdast).to.deep.equal(linkMdast);
+  });
+
+  it('should convert embeds to iframes', async () => {
+    const embedMdPath = new URL('./mocks/embed-block-adobe.md', import.meta.url);
+    const iframeMdPath = new URL('./mocks/iframe-block.md', import.meta.url);
+    const embedMd = await readFile(embedMdPath);
+    const iframeMd = await readFile(iframeMdPath);
+
+    const embedMdast = await getMdast(embedMd.toString());
+    const linkMdast = await getMdast(iframeMd.toString());
+
+    await convertEmbed(embedMdast);
+    expect(embedMdast).to.deep.equal(linkMdast);
+  });
+});
