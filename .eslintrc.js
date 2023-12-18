156db497f3f3d7f27a1f3cb6cf693f7322a8dc29
Jason Slavin <slavin@adobe.com>
2023-12-18 10:47:25 -0800

    Added linter, fixed formatting errors and added big issue files that will be refactored to ignore paths

---
+++ .eslintrc.js
@@ -0,0 +1,62 @@
+module.exports = {
+  root: true,
+  extends: ['airbnb-base', 'plugin:react-hooks/recommended', 'plugin:compat/recommended', 'plugin:ecmalist/recommended'],
+  settings: { es: { aggressive: true } },
+  env: { browser: true, mocha: true },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    allowImportExportEverywhere: true,
+    sourceType: 'module',
+    requireConfigFile: false,
+  },
+  rules: {
+    'chai-friendly/no-unused-expressions': 2,
+    'import/extensions': ['error', { js: 'always' }],
+    'import/no-cycle': 0,
+    'linebreak-style': ['error', 'unix'],
+    'no-await-in-loop': 0,
+    'no-param-reassign': [2, { props: false }],
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      {
+        selector: 'LabeledStatement',
+        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
+    'no-return-assign': ['error', 'except-parens'],
+    'no-unused-expressions': 0,
+    'object-curly-newline': ['error', {
+      ObjectExpression: { multiline: true, minProperties: 6 },
+      ObjectPattern: { multiline: true, minProperties: 6 },
+      ImportDeclaration: { multiline: true, minProperties: 6 },
+      ExportDeclaration: { multiline: true, minProperties: 6 },
+    }],
+  },
+  overrides: [
+    {
+      files: ['test/**/*.js'],
+      rules: { 'no-console': 0 },
+    },
+  ],
+  ignorePatterns: [
+    'block-report.js',
+    'blog-migration.js',
+    'migration-utils.js',
+    'fetch-utils.js',
+    'docx-utils.js',
+    'mdast-utils.js',
+    'mdast-utils-examples.js',
+    '**/mocks/*'
+  ],
+  plugins: [
+    'chai-friendly',
+  ],
+};
