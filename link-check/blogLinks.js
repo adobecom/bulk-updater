2a815bd1d523b035268730f8363f8c812ff65748
Brandon Marshall <bmarshal@adobe.com>
2024-04-17 08:25:07 -0700

    Add staged content report

--- link-check/blogLinks.js
+++ link-check/blogLinks.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import { fetch } from '@adobe/fetch';
 import { compare } from './linkCompare.js';
 import { ExcelReporter, loadListData } from '../bulk-update/index.js';
@@ -30,10 +31,62 @@ function comparison(url1, url2) {
   return url1.href === url2.href;
 }
 
-async function init() {
+async function stagedContent(list) {
+  const report = new ExcelReporter(`${pathname}reports/blog-staged-links.xlsx`, true);
+  const reportExtra = new ExcelReporter(`${pathname}reports/blog-staged-links-extra.xlsx`, false);
+  const { length } = list;
+
+  for (const [i, entry] of list.entries()) {
+    const percentage = Math.round(((i + 1) / length) * 10000) / 100;
+    console.log(`Processing entry ${i + 1} of ${length} (${percentage}%) ${entry}`);
+    const liveURL = MILO_BLOG + entry;
+    const stagedURL = liveURL.replace('/blog/', '/drafts/staged-content/blog/').replace('.hlx.live', '.hlx.page');
+
+    try {
+      const result = await compare(liveURL, stagedURL, fetch, comparison);
+      const miloLinks = result.links.map((link) => link.link1);
+      const stagedLinks = result.links.map((link) => link.link2);
+      const status = result.match ? 'match' : 'broken';
+
+      report.log('link-check', status, 'Checked Links', { liveURL, stagedURL, broken: !result.match });
+      reportExtra.log('link-check', status, 'Checked Links', {
+        entry,
+        liveURL,
+        stagedURL,
+        broken: !result.match,
+        match: result.match,
+        miloTotal: miloLinks.filter((x) => x).length,
+        stagedTotal: stagedLinks.filter((x) => x).length,
+        mismatchPercent: Math.round((result.unique.length / miloLinks.filter((x) => x).length) * 100) / 100,
+      });
+      if (!result.match) {
+        reportExtra.log('live-links', 'info', liveURL, miloLinks);
+        reportExtra.log('staged-links', 'info', stagedURL, stagedLinks);
+        reportExtra.log('links', 'milo', liveURL, miloLinks);
+        reportExtra.log('links', 'staged', stagedURL, stagedLinks);
+      }
+    } catch (e) {
+      console.error('Error Checking Links:', e);
+      report.log('link-check', 'error', 'Error Checking Links', { liveURL, stagedURL, broken: false });
+      reportExtra.log('link-check', 'error', 'Error Checking Links', {
+        entry,
+        liveURL,
+        stagedURL,
+        broken: true,
+        error: e.message,
+      });
+    }
+  }
+
+  report.generateTotals();
+  report.saveReport();
+  reportExtra.generateTotals();
+  reportExtra.saveReport();
+}
+
+async function miloFranklin(list) {
   const report = new ExcelReporter(`${pathname}reports/blog-links.xlsx`, true);
   const reportExtra = new ExcelReporter(`${pathname}reports/blog-links-extra.xlsx`, false);
-  const list = await loadListData(LIST);
   const { length } = list;
 
   for (const [i, entry] of list.entries()) {
@@ -41,6 +94,7 @@ async function init() {
     console.log(`Processing entry ${i + 1} of ${length} (${percentage}%) ${entry}`);
     const miloURL = MILO_BLOG + entry;
     const franklinURL = FRANKLIN_BLOG + entry;
+
     try {
       const result = await compare(miloURL, franklinURL, fetch, comparison);
       const miloLinks = result.links.map((link) => link.link1);
@@ -56,9 +110,14 @@ async function init() {
         match: result.match,
         miloTotal: miloLinks.filter((x) => x).length,
         franklinTotal: franklinLinks.filter((x) => x).length,
+        mismatchPercent: Math.round((result.unique.length / miloLinks.filter((x) => x).length) * 100) / 100,
       });
-      reportExtra.log('milo-links', 'info', miloURL, miloLinks);
-      reportExtra.log('franklin-links', 'info', franklinURL, franklinLinks);
+      if (!result.match) {
+        reportExtra.log('milo-links', 'info', miloURL, miloLinks);
+        reportExtra.log('franklin-links', 'info', franklinURL, franklinLinks);
+        reportExtra.log('links', 'milo', miloURL, miloLinks);
+        reportExtra.log('links', 'franklin', franklinURL, franklinLinks);
+      }
     } catch (e) {
       console.error('Error Checking Links:', e);
       report.log('link-check', 'error', 'Error Checking Links', { miloURL, franklinURL, broken: false });
@@ -76,6 +135,12 @@ async function init() {
   report.saveReport();
   reportExtra.generateTotals();
   reportExtra.saveReport();
+}
+
+async function init() {
+  const list = await loadListData(LIST);
+  await miloFranklin(list);
+  await stagedContent(list);
 }
 
 if (import.meta.url === `file://${process.argv[1]}`) {
